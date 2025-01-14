1e11a91a7e10c3ac452dbefab11b502f1064e14c
Zinnur41 <zagidullin_zin@mail.ru>
2025-01-14 14:54:43 +0300

    delete comments

--- SecondHomework.py
+++ SecondHomework.py
@@ -4,21 +4,17 @@ import dbscan
 
 pygame.init()
 
-# DBSCAN Parameters
 eps = 30
 min_pts = 3
 
-# Screen Dimensions
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Points and Colors
 points = []
 points_colors = []
 default_color = "black"
 colors = ['red', 'green', 'purple', 'yellow', 'blue']
 
-# Mouse state
 mouse_held = False
 
 running = True
@@ -30,20 +26,20 @@ while running:
             running = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 mouse_held = True
                 points.append(event.pos)
                 points_colors.append(default_color)
 
         elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 mouse_held = False
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_c:  # Clear points
+            if event.key == pygame.K_c:
                 points = []
                 points_colors = []
-            elif event.key == pygame.K_RETURN:  # Perform DBSCAN
+            elif event.key == pygame.K_RETURN:
                 cluster_labels = dbscan.dbscan_algorithm(points, eps, min_pts)
                 for i, label in enumerate(cluster_labels):
                     if label == -1:
@@ -51,14 +47,12 @@ while running:
                     else:
                         points_colors[i] = colors[label % len(colors)]
 
-    # Add points while the mouse is held down
     if mouse_held:
         pos = pygame.mouse.get_pos()
-        if not points or pos != points[-1]:  # Avoid adding duplicate points
+        if not points or pos != points[-1]:
             points.append(pos)
             points_colors.append(default_color)
 
-    # Draw points
     for i, point in enumerate(points):
         pygame.draw.circle(screen, points_colors[i], point, 5)
 
